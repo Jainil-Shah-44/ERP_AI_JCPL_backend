4348de82d183ec74534a54abeb2496a3de73a899
Jainil Shah <jainilshah330@gmail.com>
2026-04-29 15:40:00 +0530

    Revert "minor changes"
    This reverts commit 11cc7980306253576f341177e57f8e2d1658f299.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.api.purchase_requisition import router as pr_router
 from app.api.request_for_quotation import router as rfq_router
 from app.api.purchase_order import router as po_router
 from fastapi.staticfiles import StaticFiles
-from app.api.routers.dashboard import router as dashboard_router
 
 app = FastAPI(title="ERP Backend")
 
@@ -23,7 +22,6 @@ app.include_router(router.api_router)
 app.include_router(pr_router)
 app.include_router(rfq_router)
 app.include_router(po_router)
-app.include_router(dashboard_router)
 
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
